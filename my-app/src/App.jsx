0238f902dc0211d682cf46a38cb1ed6631d7ea61
aruraruri <aurilait@gmail.com>
2025-04-08 10:20:49 +0300

    Implement routing in App component and add Layout component for navbar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
 import Home from './Views/Home'
+import Profile from './Views/Profile'
+import Upload from './Views/Upload'
 
-
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Layout from './Components/Layout';
 
 function App() {
   // make a table with the mediaArray
   return (
     <>
-      <Home/>
-
+      <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/Single" element={<Home />} />
+          {/* TODO: add missing routes */}
+        </Route>
+      </Routes>
+    </Router>
     </>
   )
 }
