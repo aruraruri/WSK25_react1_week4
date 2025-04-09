625e634a45c3ddb11fbe5e4f4bf142b8ee418061
aruraruri <aurilait@gmail.com>
2025-04-09 11:17:56 +0300

    Update media fetching URL in Home component and add comment in vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
+// trailing slash is important for the base path to work correctly
 export default defineConfig({
   plugins: [react()],
   base: '/~aurila/WSK25/week4/react_routing/',
