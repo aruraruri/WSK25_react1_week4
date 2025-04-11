b593617eb141fa3c362e42c80732f709eca845d7
aruraruri <aurilait@gmail.com>
2025-04-11 08:34:35 +0300

    Refactor media fetching logic in Home component for improved clarity and error handling

--- my-app/src/Views/Home.jsx
+++ my-app/src/Views/Home.jsx
@@ -11,26 +11,41 @@ function Home() {
   console.log('Selected item:', selectedItem);
   // make a table with the mediaArray
 
+async function getMedia() {
+  try {
+    const mediaData = await fetchData(
+      import.meta.env.VITE_MEDIA_API + '/media',
+    );
+
+    console.log('mediaData', mediaData);
+
+    const uniqueUserIds = [...new Set(mediaData.map(item => item.user_id))];
+
+    console.log('uniqueUserIds', uniqueUserIds);
+
+    const authApiUrl = import.meta.env.VITE_AUTH_API;
+
+    const userData = await Promise.all(
+      uniqueUserIds.map(async (userId) => {
+      return await fetchData(`${authApiUrl}/users/${userId}`);
+  }),
+);
+
+    console.log('userData', userData);
+
+    const newData = mediaData.map((item) => {
+      const user = userData.find(({user_id}) => user_id === item.user_id);
+      return {...item, username: user.username};
+    });
+
+    setMediaArray(newData);
+
+  } catch (error) {
+    console.error('error', error);
+  }
+}
+
   useEffect(() => {
-    const getMedia = async () => {
-      try {
-        const mediaData = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
-
-        const url = import.meta.env.VITE_AUTH_API;
-        const newData = await Promise.all(mediaData.map(async (item) => {
-          const data = await fetchData(`${url}/users/${item.user_id}`);
-
-          return {...item, username: data.username};
-        }))
-        console.log(newData);
-
-        setMediaArray(newData);
-
-      } catch (error) {
-        console.error('Error fetching media:', error);
-      }
-
-    };
     getMedia();
   }, [])
 
