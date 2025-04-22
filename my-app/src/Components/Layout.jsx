55cb523608e5a83fe038b44bad41e2a3541e4dc9
aruraruri <aurilait@gmail.com>
2025-04-22 11:19:56 +0300

    feat: integrate Tailwind CSS and refactor layout components
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored the Layout component to utilize Tailwind CSS for styling the navigation.
    - Updated index.css to include Tailwind CSS and removed legacy styles.
    - Created a new Input component file for future use.
    - Removed old CSS file and cleaned up unnecessary styles.

--- my-app/src/Components/Layout.jsx
+++ my-app/src/Components/Layout.jsx
@@ -4,25 +4,26 @@ function Layout() {
   return (
     <>
 <div>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-      <li>
-        <Link to="/upload">Upload</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/logout">Logout</Link>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <nav>
+      <ul classname='overflow-hidden bg-orange-600 flex justify-end'>
+        <li>
+          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/">Home</Link>
+        </li>
+        <li>
+          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/upload">Upload</Link></li>
+        <li>
+          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/login">Login</Link>
+        </li>
+        <li>
+          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/logout">Logout</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
   <main>
     <Outlet />
   </main>
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 // trailing slash is important for the base path to work correctly
 export default defineConfig({
-  plugins: [react()],
-  base: '/~aurila/WSK25/week5/context/',
+  plugins: [tailwindcss(), react()],
+  base: '/~aurila/WSK25/week5/tailwind/',
 });
