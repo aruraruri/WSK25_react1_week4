540f01f463a935de2860cb271ba3d815c4be4878
aruraruri <aurilait@gmail.com>
2025-04-23 10:10:26 +0300

    style: update hover effects for navigation links in Layout component

--- my-app/src/Components/Layout.jsx
+++ my-app/src/Components/Layout.jsx
@@ -6,20 +6,20 @@ function Layout() {
 <div>
   <header>
     <nav>
-      <ul className='overflow-hidden bg-orange-600 flex justify-end'>
+      <ul className='overflow-hidden bg-orange-600 flex justify-end list-none p-4'>
         <li>
-          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/">Home</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:bg-[#d48c8c]" to="/">Home</Link>
         </li>
         <li>
-          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/profile">Profile</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:bg-[#d48c8c]" to="/profile">Profile</Link>
         </li>
         <li>
-          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/upload">Upload</Link></li>
+          <Link className="block text-orange-400 text-center p-4 hover:bg-[#d48c8c]" to="/upload">Upload</Link></li>
         <li>
-          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/login">Login</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:bg-[#d48c8c]" to="/login">Login</Link>
         </li>
         <li>
-          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/logout">Logout</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:bg-[#d48c8c]" to="/logout">Logout</Link>
         </li>
       </ul>
     </nav>
