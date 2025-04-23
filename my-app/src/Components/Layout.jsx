57421507b69881d259b74f6281863147475b4772
aruraruri <aurilait@gmail.com>
2025-04-23 10:05:07 +0300

    fix: correct classname attributes to className in Layout component

--- my-app/src/Components/Layout.jsx
+++ my-app/src/Components/Layout.jsx
@@ -6,20 +6,20 @@ function Layout() {
 <div>
   <header>
     <nav>
-      <ul classname='overflow-hidden bg-orange-600 flex justify-end'>
+      <ul className='overflow-hidden bg-orange-600 flex justify-end'>
         <li>
-          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/">Home</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/">Home</Link>
         </li>
         <li>
-          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/profile">Profile</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/profile">Profile</Link>
         </li>
         <li>
-          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/upload">Upload</Link></li>
+          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/upload">Upload</Link></li>
         <li>
-          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/login">Login</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/login">Login</Link>
         </li>
         <li>
-          <Link classname="block text-orange-400 text-center p-4 hover:text-orange-100" to="/logout">Logout</Link>
+          <Link className="block text-orange-400 text-center p-4 hover:text-orange-100" to="/logout">Logout</Link>
         </li>
       </ul>
     </nav>
